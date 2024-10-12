896eb97b18d63d8c7b72bfa783280b7385f2aa4a
Kotvaldo <adamstano5@gmail.com>
2024-10-12 09:53:10 +0200

    fixes

--- Aus_2_sp1/KDTree.h
+++ Aus_2_sp1/KDTree.h
@@ -3,51 +3,67 @@
 #include <vector>
 using namespace std;
 
-
 template<typename T>
 class IComparable {
 public:
-	virtual int compare(const T& other) const = 0;
-	virtual ~IComparable() = default; 
+    virtual int compare(const T& other, int cur_level) const = 0;
+    virtual ~IComparable() = default;
 };
 
-
-template <typename K, typename DataType>
+template <typename KeyType, typename DataType>
 struct KDTreeNode {
-	KDTreeNode* parent;
-	KDTreeNode* _left;
-	KDTreeNode* _right;
-	// k - pocet dimenzii
-	size_t level;
-	DataType* _data;
-	IComparable* _keys;
+    KDTreeNode* parent;
+    KDTreeNode* left;
+    KDTreeNode* right;
+    size_t level;
+    DataType* data;
+    IComparable<KeyType>* keys;  //vseobecna implementacia 
 
+    KDTreeNode(DataType* data, IComparable<KeyType>* keys, size_t level = 0)
+        : data(data), keys(keys), level(level), parent(nullptr), left(nullptr), right(nullptr) {}
 };
 
-template <typename K, typename DataType>
+template <typename KeyType, typename DataType>
 class GeneralKDTree {
-	
-
 public:
-	using KDNodeType = typename KDTreeNode<K, DataType>;
-	void clear();
-	void insert(KDNodeType* node);
-	size_t size();
+    using KDNodeType = KDTreeNode<KeyType, DataType>;
 
+    GeneralKDTree(size_t);
+
+    void clear();
+    KDNodeType* insert(DataType* data, IComparable<KeyType>* keys);
+    size_t size() const;
 
 private:
-	size_t size_;
-	KDNodeType* root;
+    size_t size_;
+    KDNodeType* root;
+    size_t k; //pocet dimensii;
 
 };
 
-template<typename K, typename DataType>
-void GeneralKDTree<K, DataType>::clear()
+template<typename KeyType, typename DataType>
+inline GeneralKDTree<KeyType, DataType>::GeneralKDTree(size_t k)
 {
+    if (k < 1) {
+        throw exception()
+    }
+
+};
+
+template<typename KeyType, typename DataType>
+void GeneralKDTree<KeyType, DataType>::clear() {
+    
 }
 
-template<typename K, typename DataType>
-void GeneralKDTree<K, DataType>::insert(KDNodeType* node)
-{
 
+
+template<typename KeyType, typename DataType>
+KDTreeNode* GeneralKDTree<KeyType, DataType>::insert(DataType* data, IComparable<KeyType>* keys) {
+    
+}
+
+
+template<typename KeyType, typename DataType>
+inline size_t GeneralKDTree<KeyType, DataType>::size() const {
+    return size_;
 }
